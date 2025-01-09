3c60c4637d2c9d649bdacd63657a138b6a264d29
PulseHealthAI <pulseteamai@gmail.com>
2025-01-08 20:16:08 -0500

    Update keypair.ts

--- src/utils/keypair.ts
+++ src/utils/keypair.ts
@@ -1,7 +1,14 @@
 import { Keypair } from "@solana/web3.js";
 import bs58 from "bs58";
 
-export const keypair = Keypair.generate();
+/**
+ * Generates a new keypair for PulseHealthAI wallet management
+ * and logs the public key and encoded secret key for secure storage.
+ */
+export const pulseHealthKeypair = Keypair.generate();
 
-console.log(keypair.publicKey.toString());
-console.log(bs58.encode(keypair.secretKey));
+console.log("PulseHealthAI Wallet Public Key:", pulseHealthKeypair.publicKey.toString());
+console.log(
+  "PulseHealthAI Wallet Secret Key (Base58 Encoded):",
+  bs58.encode(pulseHealthKeypair.secretKey)
+);
